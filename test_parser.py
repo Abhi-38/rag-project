9da92fa9c5aad32e2737ffa5328363834697dd54
Abhishek Chougule <abhishekchougule6267@gmail.com>
2026-05-21 05:35:02 +0530

    Revert "Users/abhishek/pdf parser"

--- test_parser.py
+++
@@ -1,8 +0,0 @@
-from app.ingestion.parser import DocumentParser
-
-
-text = DocumentParser.load_pdf(
-    "data/sample_docs/Ramamurthi_and_Tandon’s_Textbook_of_Neurosurgery_by_Ravi_Ramamurthi.pdf"
-)
-
-print(text[:3000])
